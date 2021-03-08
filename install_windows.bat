6bff1f454229b337cd92ec6993a2f17538d578c7
Tony Chan <tonychan2342@gmail.com>
2021-03-08 12:39:54 +0800

    fix(#5): update Windows installer script

--- install_windows.bat
+++ install_windows.bat
@@ -1,44 +1,42 @@
 @echo off
-title "mpv-discord installer script"
 
 set src_dir=%~dp0
 cd %src_dir%
 
-echo "Enter full path to the mpv folder (where mpv.exe is located)"
-set /p mpv_dir="mpv folder path: "
+set /p mpv_dir=Path: 
 
-if exist "%mpv_dir%" (
-	if not exist "%mpv_dir%\mpv.exe" (
-		echo "Failed to locate 'mpv.exe' in '%mpv_dir%'."
-		exit /b 1
-	)
+if exist %mpv_dir% (
+    if not exist %mpv_dir%\mpv.exe (
+        echo Failed to locate 'mpv.exe' in '%mpv_dir%'.
+        exit /b 1
+    )
 ) else (
-	echo "Path does not exist."
+    echo Path '%mpv_dir%' does not exist.
     exit /b 1
 )
 
-set config_dir="%mpv_dir%"
-if exist "%mpv_dir%\portable_config" (
-    set config_dir="%mpv_dir%\portable_config"
-)
+set config_dir=%mpv_dir%
+if exist %mpv_dir%\portable_config set config_dir=%mpv_dir%\portable_config
 
-set scripts_dir="%config_dir%\scripts"
-set script_opts_dir="%config_dir%\script-opts"
-if not exist "%scripts_dir%" mkdir "%scripts_dir%"
-if not exist "%script_opts_dir%" mkdir "%script_opts_dir%"
+set scripts_dir=%config_dir%\scripts
+set script_opts_dir=%config_dir%\script-opts
+if not exist %scripts_dir% mkdir %scripts_dir%
+if not exist %script_opts_dir% mkdir %script_opts_dir%
 
-echo "Copying config [discord.conf]"
-copy "%src_dir%\script-opts\discord.conf" "%script_opts_dir%" >&1
+echo Copying [discord.conf]
+copy %src_dir%\script-opts\discord.conf %script_opts_dir% >nul
 
-echo "Copying scripts [discord.lua]"
-copy "%src_dir%\scripts\discord.lua" "%scripts_dir%" >&1
+echo Copying [discord.lua]
+copy %src_dir%\scripts\discord.lua %scripts_dir% >nul
 
-echo "Copying prebuilt windows binary [mpv-discord.exe]"
-copy "%src_dir%\bin\windows\mpv-discord.exe" "%config_dir%\discord.exe" >&1
+echo Copying [mpv-discord.exe]
+copy %src_dir%\bin\windows\mpv-discord.exe %config_dir%\discord.exe >nul
 
 echo:
-echo "Done! Please manually edit the following option in the config file:"
-echo "binary_path=%config_dir%\discord.exe"
+echo Path to mpv directory: %mpv_dir%
+echo Path to config file: %script_opts_dir%/discord.conf
 echo:
-echo "Path to mpv directory: %mpv_dir%"
-echo "Path to config file: %script_opts_dir%/discord.conf"
+echo Please manually edit the following option in the config file:
+echo:
+echo   binary_path=%config_dir%\discord.exe
+echo:

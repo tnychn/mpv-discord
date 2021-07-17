d63e08b19f3b7e0a67ab8e7d63e161f742e81bd9
Martinz <martinz@martinz.dev>
2021-07-17 21:35:07 +0200

    Fix for rpc not working when Discord is installed as a snap

--- mpv-discord/discordrpc/pipe/pipe_unix.go
+++ mpv-discord/discordrpc/pipe/pipe_unix.go
@@ -10,10 +10,20 @@ import (
 )
 
 func GetPipeSocket() (net.Conn, error) {
-	path := os.Getenv("XDG_RUNTIME_DIR")
-	if path == "" {
-		path = os.TempDir()
+	tempdir := os.Getenv("XDG_RUNTIME_DIR")
+	if tempdir == "" {
+		tempdir = os.TempDir()
 	}
-	path = filepath.Join(path, "discord-ipc-0")
+	
+	path := filepath.Join(tempdir, "discord-ipc-0")
+	
+
+	// If discord is installed as a snap, it has a special tmp folder for isolation
+	_, err := os.Stat(path)
+	if os.IsNotExist(err) {
+		path = filepath.Join(tempdir, "snap.discord/discord-ipc-0")
+	}
+	//TODO: Add support for other install methods that place discord's rpc socket somewhere else
+
 	return net.DialTimeout("unix", path, time.Second*5)
 }
